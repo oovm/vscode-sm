cfa5679914b355ec6cfe5907ad93bc2693351336
Aster <galaster@foxmail.com>
2020-06-20 13:58:15 +0800

    🧨 Remove expired api from vscode

--- extension/key_map.ts
+++ extension/key_map.ts
@@ -3,46 +3,47 @@ import {
     CompletionItemProvider,
     CompletionItem,
     CompletionItemKind,
-    TextEdit,
 } from 'vscode'
 
-type Mappings = { [key: string]: string }
+type Lists = [string, string, string][]
+type Dicts = { [key: string]: string }
 
 export let EscapedInputProvider: CompletionItemProvider = {
     async provideCompletionItems(document, position, token, context) {
         let range = new Range(position.translate(0, -1), position)
         let completions: Array<CompletionItem> = []
-        for (const from in operator_maps) {
-            let to: string = operator_maps[from]
-            let item = new CompletionItem('\\' + from, CompletionItemKind.Operator)
-            item.kind = CompletionItemKind.Operator
-            item.textEdit = TextEdit.replace(range, to)
+        for (const ac in operator_maps) {
+            let to: string = operator_maps[ac]
+            let item = new CompletionItem('\\' + ac, CompletionItemKind.Operator)
+            item.detail = to
+            item.range = range
+            item.insertText = to
             completions.push(item)
         }
-        for (const from in constant_maps) {
-            let to: string = constant_maps[from]
-            let item = new CompletionItem('\\' + from, CompletionItemKind.Constant)
-            item.detail = to
-            item.textEdit = TextEdit.replace(range, to)
+        for (const ac of constant_maps) {
+            let item = new CompletionItem('\\' + ac[0], CompletionItemKind.Constant)
+            item.detail = ac[2] + " " + ac[1]
+            item.range = range
+            item.insertText = ac[1]
             completions.push(item)
         }
         return completions
     },
 }
 
-const constant_maps: Mappings = {
-    'empty': '∅',
-    'mathbb{P}': 'ℙ',
-    'mathbb{C}': 'ℂ',
-    'mathbb{N}': 'ℕ',
-    'mathbb{Q}': 'ℚ',
-    'mathbb{R}': 'ℝ',
-    'mathbb{Z}': 'ℤ',
-}
+const constant_maps: Lists = [
+    ['empty', '∅', 'std::set::Empty'],
+    ['mathbb{P}', 'ℙ', 'std::set::Prime'],
+    ['mathbb{N}', 'ℕ', 'std::set::Nat'],
+    ['mathbb{Z}', 'ℤ', 'std::set::Integer'],
+    ['mathbb{Q}', 'ℚ', 'std::set::Q'],
+    ['mathbb{R}', 'ℝ', 'std::set::Reals'],
+    ['mathbb{C}', 'ℂ', 'std::set::Complex'],
+]
 
-const operator_maps: Mappings = {
-    "`": "",
-    "``": "",
+const operator_maps: Dicts = {
+    "`": "¯",
+    "``": "¨",
     '<<': '⟪',
     '>>': '⟫',
     '.': '∙',
